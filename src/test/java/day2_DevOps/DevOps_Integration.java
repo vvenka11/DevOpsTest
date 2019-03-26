3303d4ef3908659e43c764133b9c1296e6b811d0
Venkatakrishnan <v.venkatakrishnan@accenture.com>
2019-03-26 01:56:40 -0500

    Adding new files

---
+++ src/test/java/day2_DevOps/DevOps_Integration.java
@@ -0,0 +1,10 @@
+package day2_DevOps;
+
+import org.testng.annotations.Test;
+
+public class DevOps_Integration {
+  @Test
+  public void firstDevOpsTest() {
+	  System.out.println("Test Executed");
+  }
+}
